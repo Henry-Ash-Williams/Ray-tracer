460dde5ee46ef4de5f65dda1bf8833a203b1f4fd
Henry Williams <henrywilliams95000@gmail.com>
2020-04-23 12:00:35 +0100

    Worked on file structure and intersection trait

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,36 @@
 #![allow(dead_code, unused_imports, unused_variables)]
 extern crate image;
 extern crate nalgebra as na;
-
-mod constants;
-mod sphere;
+extern crate utils;
 
 use std::{
     error::Error
 };
 
+use utils::{
+    constants,
+    shapes::* 
+};
+
 use na::Vector3;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let mut scene: Vec<sphere::Sphere> = Vec::new();
-
+    // Create the image buffer with a 1080p resolution (1920x1080)
     let mut image = image::ImageBuffer::new(
         constants::IMAGE_WIDTH, constants::IMAGE_HEIGHT
     );
 
-    scene.push(sphere::Sphere::new(
-        Vector3::new(157f32, 1011f32, 414f32),
-        5.0,
-        (0x8Du8, 0x61u8, 0xEDu8)
-    ));
+    let scene: Vec<dyn Intersection> = Vec::new();
 
     // Fills the image with a background
     for y in 0 .. constants::IMAGE_HEIGHT {
         for x in 0 .. constants::IMAGE_WIDTH {
             let pixel = image.get_pixel_mut(x, y);
-            let image::Rgb(_data) = *pixel;
             *pixel = image::Rgb(constants::IMAGE_BACKGROUND);
         }
     }
 
-    image.save("image.png")?;
+    image.save(constants::FILENAME)?;
 
     Ok(())
 }
