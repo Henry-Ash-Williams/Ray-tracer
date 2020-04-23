079cc7df91893892fb2114fb2a2d8fc9da208b0f
Henry Williams <henrywilliams95000@gmail.com>
2020-04-23 15:19:12 +0100

    Scene

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,18 @@
 #![allow(dead_code, unused_imports, unused_variables)]
 extern crate image;
-extern crate nalgebra as na;
 extern crate utils;
 
 use std::{
     error::Error
 };
 
+mod scene;
+
 use utils::{
     constants,
-    shapes::* 
+    shapes::*,
+    Vector3
 };
-
-use na::Vector3;
 
 fn main() -> Result<(), Box<dyn Error>> {
     // Create the image buffer with a 1080p resolution (1920x1080)
@@ -21,6 +21,18 @@ fn main() -> Result<(), Box<dyn Error>> {
     );
 
     let scene: Vec<Box<dyn Intersection>> = Vec::new();
+
+    scene.push(Box::new(sphere::Sphere::new(
+        Vector3::new(2.01009f32, 2.3031f32, 0.466287f32),
+        1f32, 
+        (0x66u8, 0x00u8, 0xFFu8)
+    )));
+    
+    scene.push(Box::new(cube::Cube::new(
+        Vector3::new(2.01009f32, 2.3031f32, 0.466287f32),
+        1f32, 
+        (0x66u8, 0x00u8, 0xFFu8)
+    )));
 
     // Fills the image with a background
     for y in 0 .. constants::IMAGE_HEIGHT {
---
+++ src/scene.rs
@@ -0,0 +1,64 @@
+extern crate image;
+extern crate utils;
+
+use utils::{
+    constants,
+    shapes::*,
+    Vector3
+};
+
+pub struct Scene {
+    UpDirection: Vector3<f32>,
+    LeftDirection: Vector3<f32>,
+    LookDirection: Vector3<f32>,
+    EyePosition: Vector3<f32>,
+    MeshTopLeft: Vector3<f32>,
+    MeshDistance: f32,
+    PixelWidth: u32,
+    ScreenWidth: u32,
+    ScreenHeight: u32,
+    scene_objects: Vec<Box<dyn Intersection>> 
+}
+
+impl Scene {
+    pub fn new(
+        LeftDirection: Vector3<f32>,
+        LookDirection: Vector3<f32>,
+        EyePosition: Vector3<f32>,
+        MeshDistance: f32,
+        PixelWidth: u32,
+        ScreenWidth: u32,
+        ScreenHeight: u32,
+    ) -> Scene {
+        let UpDirection = LookDirection.cross(&LeftDirection).normalize();
+        let MeshTopLeft = EyePosition + (MeshDistance * LookDirection) + ((PixelWidth * ScreenWidth / 2) as f32 * LeftDirection) + PixelWidth as f32;
+        
+        Scene {
+            UpDirection: UpDirection,
+            LeftDirection: LeftDirection,
+            LookDirection: LookDirection,
+            EyePosition: EyePosition,
+            MeshTopLeft: MeshTopLeft,
+            MeshDistance: MeshDistance,
+            PixelWidth: PixelWidth,
+            ScreenWidth: ScreenWidth,
+            ScreenHeight: ScreenHeight,
+            scene_objects: Vec::new()
+        }
+    }
+
+    pub fn add_scene_object<'a, O>(&mut self, object: O) where O: Intersection + Clone {
+        self.scene_objects.push(Box::new(object));
+    } 
+
+    pub fn GetRayToMesh(&self, x: u32, y: u32) -> Vector3<f32> {
+        let pixel_pos = self.MeshTopLeft - ((self.PixelWidth * x) as f32 * self.LeftDirection) - ((self.PixelWidth * y) as f32 * self.UpDirection);
+        let ray = (pixel_pos - self.EyePosition).normalize();
+
+        ray
+    }
+
+    pub fn get_ray_to_mesh(&self, x: u32, y: u32) -> Vector3<f32> {
+        unimplemented!()
+    }
+}
